7fcec4bc0829b877aadd14c10a943a84eac3431d
Prachi <prachi@fugenx.com>
2019-06-14 15:56:01 +0530

    Initial commit

---
+++ HopeTest/Controller/ShareViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ShareViewController.swift
+//  HopeTest
+//
+//  Created by Sushma on 14/06/19.
+//  Copyright © 2019 Fugenx. All rights reserved.
+//
+
+import UIKit
+
+class ShareScanViewController: UIActivityViewController {
+    
+    init(sourceView: UIView, sharedObject: Any) {
+        super.init(activityItems: [sharedObject], applicationActivities: nil)
+        
+        // Set up popover presentation style
+        modalPresentationStyle = .popover
+        popoverPresentationController?.sourceView = sourceView
+        popoverPresentationController?.sourceRect = sourceView.bounds
+        
+        self.excludedActivityTypes = [.markupAsPDF, .openInIBooks, .message, .print,
+                                      .addToReadingList, .saveToCameraRoll, .assignToContact,
+                                      .copyToPasteboard, .postToTencentWeibo, .postToWeibo,
+                                      .postToVimeo, .postToFlickr, .postToTwitter, .postToFacebook]
+    }
+    
+    deinit {
+        // Restart the session in case it was interrupted by the share sheet
+        if let configuration = ViewController.instance?.sceneView.session.configuration,
+            ViewController.instance?.state == .testing {
+            ViewController.instance?.sceneView.session.run(configuration)
+        }
+    }
+}
